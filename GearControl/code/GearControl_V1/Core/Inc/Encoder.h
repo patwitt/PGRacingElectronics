a8255906e99057a5415f545488a48bd5e9ec469e
patwitt <wittbrodt.patryk.@gmail.com>
2022-05-10 23:22:19 +0200

    Add code for GearControl V1 (2021 working)

---
+++ GearControl/code/GearControl_V1/Core/Inc/Encoder.h
@@ -0,0 +1,16 @@
+/*
+ * Encoder.h
+ *
+ *  Created on: 17.04.2021
+ *      Author: User
+ */
+
+#ifndef INC_ENCODER_H_
+#define INC_ENCODER_H_
+
+#include "main.h"
+
+void Encoder_Process(void);
+boolean Encoder_Init(void);
+
+#endif /* INC_ENCODER_H_ */
