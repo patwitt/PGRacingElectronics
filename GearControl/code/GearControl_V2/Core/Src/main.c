8e1ee42c8dc8f18010c0862d866ce1adac611f74
Patryk Wittbrodt <wittbrodt.patryk@gmail.com>
2022-08-08 03:33:42 +0200

    fix PWM

--- GearControl/code/GearControl_V2/Core/Src/main.c
+++ GearControl/code/GearControl_V2/Core/Src/main.c
@@ -667,9 +667,9 @@ static void MX_TIM3_Init(void)
 
   /* USER CODE END TIM3_Init 1 */
   htim3.Instance = TIM3;
-  htim3.Init.Prescaler = 19999;
+  htim3.Init.Prescaler = 83;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 83;
+  htim3.Init.Period = 19999;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_PWM_Init(&htim3) != HAL_OK)
